365816264319526f777223fb6be6ff9711d93bd8
sinsit1 <alexandra8989@gmail.com>
2018-07-19 23:22:37 +0200

    add mobile functionality

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 'use strict';
+const keys = document.querySelectorAll('.key');
 
+// para tocar con el tactil del movil
+function tocar (e) {
+
+  const targetElement = e.currentTarget;
+  const audio = document.querySelector(`audio[data-key= "${targetElement.getAttribute('data-key')}"]`);
+  const key = document.querySelector(`.key[data-key= "${targetElement.getAttribute('data-key')}"]`);
+
+
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+}
+
+//para tocar con las teclas del pc
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key= "${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key= "${e.keyCode}"]`);
@@ -10,17 +26,17 @@ function playSound(e) {
   key.classList.add('playing');
 }
 
+//para eliminar transicion y que vuelvan al sitio despues del sonido
 function removeTransition(e) {
 
   if (e.propertyName !== 'transform') return; // dont do anything if is not a tranform
   this.classList.remove('playing');
 }
-const keys = document.querySelectorAll('.key');
 
-keys.forEach (key => key.addEventListener('click', removeTransition))
+//listener para clicks de movil
+keys.forEach (key => key.addEventListener('click', tocar)),
 
-keys.forEach(key => key.addEventListener('transitionend', removeTransition))
-
+keys.forEach(key => key.addEventListener('transitionend', removeTransition)),
 
 window.addEventListener('keydown', playSound);
 
